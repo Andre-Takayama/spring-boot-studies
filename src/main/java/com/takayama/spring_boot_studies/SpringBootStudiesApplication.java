e842842bccdc75cafe212f392348e8462c7f900b
Andre-Takayama <andreluiztakayamaoliveira@gmail.com>
2024-09-02 08:44:12 -0400

    Initial commit

---
+++ src/main/java/com/takayama/spring_boot_studies/SpringBootStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.takayama.spring_boot_studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takayama/spring_boot_studies/SpringBootStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takayama.spring_boot_studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
